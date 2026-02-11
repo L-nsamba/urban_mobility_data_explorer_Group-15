f9f92b5df483ab7c43d6356c4fa99886e73d6087
L-nsamba <l.nsamba@alustudent.com>
2026-02-11 18:14:51 +0200

    separating the cleaning and excluding logic & running creation test

--- etl/etl_pipeline.py
+++ etl/etl_pipeline.py
@@ -1,54 +1,11 @@
-import pandas as pd
+from trip_cleaning import clean_data
+from excluded_logs import split_transactions
 
-#Reading and accessing the dataset path
-df = pd.read_csv("etl/raw_data/train.csv")
+# Cleaning data
+df = clean_data("etl/raw_data/train.csv")
 
-# Dropping rows if missing critical field
-df = df.dropna(subset=["pickup_datetime", "dropoff_datetime", "pickup_longitude", "pickup_latitude"])
+# Separating the clean from the erroneous data
+cleaned_df, excluded_df = split_transactions(df)
 
-#Removing outlier (passenger_count < 0 & passenger_count >= 8)
-df = df[(df["passenger_count"] > 0) & (df["passenger_count"] <= 8)]
-
-
-#Normalizing timestamp & formatting timestamp, coordinates and numeric fields
-#Co-ordinate Configurations
-DECIMAL_PLACES = 4  # Accuracy only drops to about 11 metres.
-
-#Read CSV
-#print(f"Reading INPUT_FILE...")
-#df = pd.read_csv("etl/raw_data/train.csv")
-#print(f"Loaded {len(df):,} rows")
-
-#Normalize the timestamps to YYYY-MM-DD HH:MM:SS
-df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime']).dt.strftime('%Y-%m-%d %H:%M:%S')
-df['dropoff_datetime'] = pd.to_datetime(df['dropoff_datetime']).dt.strftime('%Y-%m-%d %H:%M:%S')
-
-#Round the coordinates to 4 decimal places
-df['pickup_longitude'] = df['pickup_longitude'].round(DECIMAL_PLACES)
-df['pickup_latitude'] = df['pickup_latitude'].round(DECIMAL_PLACES)
-df['dropoff_longitude'] = df['dropoff_longitude'].round(DECIMAL_PLACES)
-df['dropoff_latitude'] = df['dropoff_latitude'].round(DECIMAL_PLACES)
-
-#Validate longitude range (-180 to 180)
-lon_invalid = ((df['pickup_longitude'] < -180) | (df['pickup_longitude'] > 180) | (df['dropoff_longitude'] < -180) | (df['dropoff_longitude'] > 180))
-print(f"Invalid longitudes: {lon_invalid.sum()}")
-
-#Validate latitude range (-90 to 90)
-lat_invalid = ((df['pickup_latitude'] < -90) | (df['pickup_latitude'] > 90) | (df['dropoff_latitude'] < -90) | (df['dropoff_latitude'] > 90))
-print(f"Invalid latitudes: {lat_invalid.sum()}")
-
-
-#Create and justify atleast additional (1) derived features
-#1. My ideas (pickup_days & pickup_hours to analyze rush hour) 
-#   free to create others but ensure there's a logical connection
-#   between existing elements
-
-# Creating derived quantity of pickup hour to analyze rush hours
-df["pickup_hour"] = pd.to_datetime(df["pickup_datetime"]).dt.hour
-
-#Creating derived quantity of pick up day to analyze weekday vs weekend traffic
-df["pickup_dayofweek"] = pd.to_datetime(df["pickup_datetime"]).dt.day_name()
-
-#Log for excluded transactionns
-
-#Saving cleaned dataset(etl/processed/clean.csv) & excluded data (etl/excluded_logs/excluded.csv)
+cleaned_df.to_csv("etl/processed_data/clean.csv", index=False)
+excluded_df.to_csv("etl/processed_data/excluded.csv", index=False)
