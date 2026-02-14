1238d25c87119ab8ed69b260a5b8618a5cde98e8
L-nsamba <l.nsamba@alustudent.com>
2026-02-14 16:07:34 +0200

    Establishing database connection to the csv data

--- database/database_setup.py
+++ database/database_setup.py
@@ -31,8 +31,8 @@ class Trip(Base):
     __tablename__ = "trips"
 
     trip_id = Column(Integer, primary_key=True, autoincrement=True)
-    tpep_pickup_datetime = Column(String(50))
-    tpep_dropoff_datetime = Column(String(50))
+    tpep_pickup_datetime = Column(DateTime)
+    tpep_dropoff_datetime = Column(DateTime)
     passenger_count = Column(Integer)
     trip_distance = Column(Float)
     fare_amount = Column(Float)
@@ -45,40 +45,8 @@ class Trip(Base):
 
     trip_duration_min = Column(Float)
     fare_per_km = Column(Float)
-    average_speed_km_h = Column(Float)
+    average_speed_kmh = Column(Float)
 
 Base.metadata.create_all(engine)
 
 print("Database connected and table created")
-
-# # 3. Read clean csv
-
-# df = pd.read_csv("clean.csv")
-
-# # 4. Insert data
-
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-# for index, row in df.iterrows():
-#     trip = Trip(
-#         tpep_pickup_datetime=row["tpep_pickup_datetime"],
-#         tpep_dropoff_datetime=row["tpep_dropoff_datetime"],
-#         passenger_count=row["passenger_count"],
-#         trip_distance=row["trip_distance"],
-#         fare_amount=row["fare_amount"],
-#         tip_amount=row["tip_amount"],
-#         total_amount=row["total_amount"],
-#         PULocationID=row["PULocationID"],
-#         DOLocationID=row["DOLocationID"],
-#         trip_duration_min=row["trip_duration_min"],
-#         fare_per_km=row["fare_per_km"],
-#         average_speed_km_h=row["average_speed_km_h"]
-#     )
-
-#     session.add(trip)
-
-# session.commit()
-# session.close()
-
-# print("Data inserted successfully!")
--- etl/etl_pipeline.py
+++ etl/etl_pipeline.py
@@ -1,7 +1,13 @@
 from trip_cleaning import clean_data
 from excluded_logs import split_transactions
 from integration import integrate_data
+from sqlalchemy import create_engine
+from sqlalchemy import text
+import os
+from dotenv import load_dotenv
+import pandas as pd
 
+load_dotenv()
 # Cleaning data
 df = clean_data("etl/raw_data/yellow_tripdata_2019-01.parquet")
 
@@ -16,6 +22,44 @@ integrated_df, zones = integrate_data(
     "etl/raw_data/taxi_zones/taxi_zones.shp"
 )
 
-cleaned_df.to_csv("etl/processed_data/clean.csv", index=False)
-excluded_df.to_csv("etl/processed_data/excluded.csv", index=False)
-integrated_df.to_csv("etl/processed_data/integrated.csv", index=False)
+# Creation of database connection
+engine = create_engine(
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+    connect_args={
+        "ssl" : {"ca": os.getenv("DB_CA")}
+    }
+)
+
+# Injecting the zones csv data into the zones table
+zones_df = pd.read_csv("etl/raw_data/taxi_zone_lookup.csv")
+
+# Error handling to ensure correct id initial reference upon data entry
+with engine.connect() as conn:
+    count = conn.execute(text("SELECT COUNT (*) FROM zones")).scalar()
+
+if count == 0:
+    zones_df.to_sql("zones", engine, if_exists="append", index=False, chunksize=10000)
+    
+# Defining of the exact columns to enter into the Trip table before being passed into the db
+trip_columns = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime",
+    "passenger_count",
+    "trip_distance",
+    "fare_amount",
+    "tip_amount",
+    "total_amount",
+    "PULocationID",
+    "DOLocationID",
+    "trip_duration_min",
+    "fare_per_km",
+    "average_speed_kmh"
+]
+
+# Filtering out only the columns we want to be displayed
+trips_df = cleaned_df[trip_columns]
+
+# Injecting the cleaned data into the database
+trips_df.to_sql("trips", engine, if_exists="append", index=False, chunksize=10000)
+print("Data successfully injected into database!")
