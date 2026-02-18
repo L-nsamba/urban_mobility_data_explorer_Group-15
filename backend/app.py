1fc55b89c2fb0c3dc749466f7ff57dadec66bdca
L-nsamba <l.nsamba@alustudent.com>
2026-02-18 16:23:02 +0200

    html and css polishing and CORS installation

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from api.get_trips_per_day import trips_day_blueprint
 from api.get_trips_per_hour import trips_hour_blueprint
 from api.get_distance_per_day_per_borough import distance_blueprint
@@ -7,7 +8,8 @@ from api.get_avg_speed_per_day import avg_speed_blueprint
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
+    
     # Registering blueprints with a common prefix of "/api"
     app.register_blueprint(trips_day_blueprint, url_prefix="/api")
     app.register_blueprint(trips_hour_blueprint, url_prefix="/api")
